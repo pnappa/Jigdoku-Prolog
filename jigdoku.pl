d47d7baf2067a0bd8bdf4b791ae7adddaf211fd0
Patrick Nappa <patricknappa@gmail.com>
2016-09-03 16:07:46 +1000

    intersect done

--- jigdoku.pl
+++ jigdoku.pl
@@ -1,50 +1,24 @@
-same(X, X).
+% Patrick Nappa
+% 440243449
 
+% TODO: test intersect row
 % if any of the cells are the same
-intersectrow([HR0|TR0], [HR0|TR1]).
-intersectrow([HR0|TR0], [HR1|TR1]) :- intersectrow([HR0|TR0], TR1).
-intersectrow([HR0|TR0], [HR1|TR1]) :- intersectrow(TR0, [HR1|TR1]).
+intersectrow([HR0|_], [HR0|_]).
+intersectrow([HR0|TR0], [_|TR1]) :- intersectrow([HR0|TR0], TR1).
+intersectrow([_|TR0], [HR1|TR1]) :- intersectrow(TR0, [HR1|TR1]).
 
+% TODO: test intersect grid
 % check every combination of rows if they intersect
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R1).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R2).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R3).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R4).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R5).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R6).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R7).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R0, R8).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R1, R2).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R1, R3).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R1, R4).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R1, R5).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R1, R6).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R1, R7).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R1, R8).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R2, R3).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R2, R4).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R2, R5).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R2, R6).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R2, R7).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R2, R8).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R3, R4).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R3, R5).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R3, R6).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R3, R7).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R3, R8).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R4, R5).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R4, R6).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R4, R7).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R4, R8).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R5, R6).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R5, R7).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R5, R8).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R6, R7).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R6, R8).
-intersectgrid([R0,R1,R2,R3,R4,R5,R6,R7,R8]) :- intersectrow(R7, R8).
+intersectgrid([H |[TH | _]]) :- intersectrow(H, TH).
+intersectgrid([H |[_ | TT]]) :- intersectgrid([H | TT]).
+intersectgrid([_ |[TH | TT]]) :- intersectgrid([TH | TT]).
+
+% wh
+makesgrid(X) :-
+
 
 % check first all subgrids dont intersect
 % then unioning them generates 9x9 grid
-completegrid(X) :- not(intersectgrid(X)), makesgrid(X).
+% completegrid(X) :- not(intersectgrid(X)), makesgrid(X).
 
 
